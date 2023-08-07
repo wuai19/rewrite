fec22d380d4dfb29f2cf67c689546f3a3437e94a
wuai19 <141476049+wuai19@users.noreply.github.com>
2023-08-07 14:20:51 +0800

    Create kuwo.js

---
+++ kuwo.js
@@ -0,0 +1,35 @@
+try {
+    var body = $response.body;
+    var url = $request.url;
+
+    if (typeof body === 'string' && typeof url === 'string') {
+        var obj = JSON.parse(body);
+
+        const vip = '/vip/v2/user/vip';
+        const time = '/vip/spi/mservice';
+
+        if (url.indexOf(vip) != -1) {
+            obj.data["isNewUser"] = "2";
+            obj.data["vipLuxuryExpire"] = "1835312949000";
+            obj.data["time"] = "1961170340993";
+            obj.data["isYearUser"] = "2";
+            obj.data["vipmExpire"] = "1835312949000";
+            obj.data["vipOverSeasExpire"] = "1835312949000";
+            obj.data["vipExpire"] = "1835312949000";
+            obj.data["vip3Expire"] = "1835312949000";
+            body = JSON.stringify(obj);
+        }
+
+        if (url.indexOf(time) != -1) {
+            obj["isVIPMAutoPay"] = 2;
+            obj["isVIPLuxAutoPay"] = 2;
+            body = JSON.stringify(obj);
+        }
+
+        $done({body});
+    } else {
+        console.log('Unexpected types for url or body');
+    }
+} catch (error) {
+    console.error('An error occurred:', error);
+}
