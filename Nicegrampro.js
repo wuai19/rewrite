3381319372ebbfbe5871714be936c358c9a59030
wuai19 <141476049+wuai19@users.noreply.github.com>
2023-08-07 14:04:27 +0800

    Create Nicegrampro.js

---
+++ Nicegrampro.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：Nicegram: Unlimited Text+解锁订阅
+下载地址：
+软件版本：1.1.2
+脚本作者：彭于晏💞
+更新时间：2022-10-8
+使用声明：⚠️此脚本仅供学习与交流，请勿贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/wuai19/rewrite/main/Nicegram.js
+
+[mitm] 
+hostname=restore-access.indream.app
+
+
+*******************************/
