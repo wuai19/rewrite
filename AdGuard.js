807ae6cd50ef4018f86e0340935a6d1082b04864
小坤 <141476049+wuai19@users.noreply.github.com>
2023-08-08 12:05:38 +0800

    Update AdGuard.js

--- AdGuard.js
+++ AdGuard.js
@@ -17,4 +17,8 @@ hostname = mobile-api.adguard.org
 *************************************/
 
 
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('3 0=1.4($5.2);0={"6":[{"7":"8","9":"a.b.c"}]}$d({2:1.e(0)});',15,15,'chxm1023|JSON|body|var|parse|response|products|premium_status|ACTIVE|product_id|com|adguard|lifetimePurchase|done|stringify'.split('|'),0,{}))
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {"products":[{"premium_status":"ACTIVE","product_id":"com.adguard.lifetimePurchase"}]}
+
+$done({body : JSON.stringify(chxm1023)});
