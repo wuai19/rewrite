b33569238c9c97f08a2a5edd76a56e1c06fd5675
小坤 <141476049+wuai19@users.noreply.github.com>
2023-08-08 20:11:55 +0800

    Update kw.js

--- kw.js
+++ kw.js
@@ -1,15 +1,12 @@
 /* 
 脚本功能：完美解锁酷我音乐
-脚本作者：king
-下载地址：https://is.gd/CWqKMX
+脚本作者：转载于king
 更新时间：2022.8.16
-脚本频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
 使用声明：⚠️⚠️⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️⚠️
+        请勿贩卖！⚠️⚠️⚠️
 
 代码重构:这版是 最完美！！！的酷我解锁
-当前版本:10.2.8
+当前版本:10.4.6
 [rewrite_local]
 # 完美解锁酷我音乐
 ^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/wuai19/rewrite/main/kw.js
