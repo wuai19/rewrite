47ef2768e5a62edb3472e77e1ecbc92818599084
小坤 <141476049+wuai19@users.noreply.github.com>
2023-08-09 08:52:27 +0800

    Update and rename dejianxiaoshuo.js to Dejianxiaoshuo.js

--- Dejianxiaoshuo.js
+++ Dejianxiaoshuo.js
@@ -8,7 +8,7 @@
 
 [rewrite_local]
 
-^https:\/\/dj\.palmestore\.com\/zyuc\/api\/user\/accountInfo url script-response-body https://raw.githubusercontent.com/wuai19/rewrite/main/dejianxiaoshuo.js
+^https:\/\/dj\.palmestore\.com\/zyuc\/api\/user\/accountInfo url script-response-body https://raw.githubusercontent.com/wuai19/rewrite/main/Dejianxiaoshuo.js
 
 [mitm]
 
