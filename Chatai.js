c0e72fe7f2ea97efa191d7fe3504bb3485a23ffc
小坤 <141476049+wuai19@users.noreply.github.com>
2023-08-09 08:51:38 +0800

    Update and rename chatai.js to Chatai.js

--- Chatai.js
+++ Chatai.js
@@ -11,7 +11,7 @@
 
 [rewrite_local]
 
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/wuai19/rewrite/main/chatai.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/wuai19/rewrite/main/Chatai.js
 
 [mitm]
 
